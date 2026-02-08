6711be4887f52ff503e2542d2f14a57b2075a56d
ronny7-code <bijay98813@gmail.com>
2026-02-08 18:00:45 +0545

    working on login and signup

---
+++ src/main/java/com/JCode/Gym_Buddy/controller/LoginSignupController.java
@@ -0,0 +1,18 @@
+package com.JCode.Gym_Buddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginSignupController {
+
+     @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+    
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+}
